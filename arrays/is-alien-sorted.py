682cd65c4d4c0d545da190214a2b9c9016dd26c2
harshalnikhare <harshaln369@gmail.com>
2025-11-18 09:11:00 +0530

    is alien sorted

--- arrays/is-alien-sorted.py
+++ arrays/is-alien-sorted.py
@@ -1,2 +1,31 @@
 def isAlienSorted(words, order):
-    pass
+    dict = {}
+
+    for i, char in enumerate(order):
+        dict[char] = i
+
+    for i in range(len(words) - 1):
+
+        found_difference = False
+        curr_word = words[i]
+        next_word = words[i + 1]
+
+        for j in range(min(len(curr_word), len(next_word))):
+
+            if dict[curr_word[j]] > dict[next_word[j]]:
+                return False
+
+            if dict[curr_word[j]] < dict[next_word[j]]:
+                found_difference = True
+                break
+
+        if not found_difference and len(curr_word) > len(next_word):
+            return False
+
+    return True
+
+
+print(isAlienSorted(["hello", "leetcode"], "hlabcdefgijkmnopqrstuvwxyz"))
+print(isAlienSorted(["word", "world", "row"], "worldabcefghijkmnpqstuvxyz"))
+print(isAlienSorted(["apple", "app"], "abcdefghijklmnopqrstuvwxyz"))
+print(isAlienSorted(["z", "apple", "app"], "zabcdefghijklmnopqrstuvwxy"))
